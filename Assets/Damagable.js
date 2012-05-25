3178715a147252a3cc23f4f3780e06588d8a3f67
Mark Nutter <marknutter@gmail.com>
2012-05-24 22:20:02 -0500

    initial commit

---
+++ Assets/Damagable.js
@@ -0,0 +1,25 @@
+#pragma strict
+
+var Health:float = 100;
+var Armor:float = 100;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function Damage (damage : float) {
+
+	Health = Health - damage;
+	if( Health < 0)
+		Health = 0;
+}
+
+function Die () {
+
+	if(Health <= 0)
+		Destroy(gameObject);
+}
